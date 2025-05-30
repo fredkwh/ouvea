a967015b3c857000d1cef3f11f81747daf5147af
fredkwh <fred.kwh@gmail.com>
2025-05-29 20:41:03 -0400

    Fix import firebaseconfig (casse Linux)

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import './firebase/firebaseConfig' // 🔥 Initialisation Firebase obligatoire
+import './firebase/firebaseconfig' // 🔥 Initialisation Firebase obligatoire
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
